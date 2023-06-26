6a64e8e7315edcd80b525cc23ba45ef2ca69da2f
Sidiki-del <sidikiissadiarra@gmail.com>
2023-06-26 12:39:20 +0000

    Edit and Delete Post

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,10 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function (){
     Route::get('posts', [App\Http\Controllers\Admin\PostController::class, 'index']);
     Route::get('add_post', [App\Http\Controllers\Admin\PostController::class, 'create']);
     Route::post('add_post', [App\Http\Controllers\Admin\PostController::class, 'store']);
-
+    Route::get('post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
+    Route::put('update_post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
+    Route::get('delete_post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
 });
+
+
+
