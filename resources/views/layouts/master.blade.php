7805fb17d9881cb60782570787093eead41c50c4
Sidiki-del <sidikiissadiarra@gmail.com>
2023-06-27 19:45:54 +0000

    Summernote Added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,10 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{asset('assets/css/styles.css')}}" rel="stylesheet" />
+
+    {{-- Summernote CSS LINKS --}}
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
     <!-- Scripts -->
@@ -32,8 +36,17 @@
          @include('layouts.inc.admin-footer')
         </div>
 </div>
+    <script src=" {{ asset('assets/js/jquery-3.6.0.min.js') }}" ></script>
     <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('assets/js/scripts.js')}}"></script>
 
+     {{-- Summernote JS LINK --}}
+     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+     <script>
+ $(document).ready(function() {
+     $("#mySummernote").summernote();
+     $('.dropdown-toggle').dropdown();
+ });
+</script>
 </body>
 </html>
