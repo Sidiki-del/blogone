05f751b5b734760fa1368774296b03f563c66b96
Sidiki-del <sidikiissadiarra@gmail.com>
2023-06-29 12:04:22 +0000

    Fectching Users

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function (){
     Route::get('post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
     Route::put('update_post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
     Route::get('delete_post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
+    Route::get('/users', [App\Http\Controllers\Admin\UserController::class, 'index']);
+
 });
 
 
